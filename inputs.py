dd8592557eaa8b5218e4921890783d2d11409891
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2026-05-02 10:46:29 +0530

    tcs input handling s1

--- inputs.py
+++ inputs.py
@@ -1,1 +1,36 @@
 # types of input taking dynamically use strip,slice,replace etc
+
+
+# data="[1,2,3,4]"
+# tcs_input=list(map(int,data.strip("[]").split(",")))
+# print(tcs_input)
+
+
+# data=int(input("Enter a number:"))
+
+# data=input("Enter your name")
+# print(data)
+
+# raw='1,"5",6,'
+# data=list(map(int,raw.strip(',').replace('\"','').split(",")))
+# print(data)
+
+
+
+# for i in raw:
+#     if type(i)==int:
+#         print(i)
+# try:
+#     if raw[-1]==
+#     data=list(map(int,raw.split(",")))
+
+
+# print(*data)
+
+
+# raw=[5,10,20,30,40,50]
+
+# n=raw[0]
+# ele=raw[1:]
+# print(n)
+# print(ele)
