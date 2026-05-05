03754f76d0742254dc70186893cc0002788d272a
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2026-05-05 22:56:10 +0530

    reversing number

--- problems.py
+++ problems.py
@@ -28,19 +28,31 @@
 
 # checking prime number or not 
 
-given="""[10,"20",5]"""
+# given="""[10,"20",5]"""
 
-data=list(map(int,given.strip("[]").replace('"','').split(",")))
+# data=list(map(int,given.strip("[]").replace('"','').split(",")))
 
-print(*data)
-for i in data:
-    flag=1
-    for j in range(2,i):
-        if i%j==0:
-            flag=0
-            break
-    if flag:
-        print(i," is prime")
+# print(*data)
+# for i in data:
+#     flag=1
+#     for j in range(2,i):
+#         if i%j==0:
+#             flag=0
+#             break
+#     if flag:
+#         print(i," is prime")
+
+# reversing a number
+
+given=int(input("Enter a number:"))
+res=0
+while given>0:
+    rem=given%10
+    given//=10
+    res=rem+res*10
+print(res)
+
+
 
 
 
